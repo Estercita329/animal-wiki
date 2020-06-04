38978ef5f65512cbc5c5fac8037cd5d2aac8e02d
Spencer Moody <spencer.moody@outlook.com>
2020-06-04 11:37:03 -0700

    Add UI
    Co-authored-by: Michael Watts <Wattsjmichael@gmail.com>
    Co-authored-by: Ester Tataje <ester.tataje@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,30 @@ $(document).ready(function(){
       $("#quokka").show();
       $("#liger").hide();
       $("#sloth").hide();
+      $("#ligerClick").hide();
+      $("#slothClick").hide();
     } else if (animal === "liger") {
       $("#liger").show();
       $("#quokka").hide();
       $("#sloth").hide();
+      $("#quokkaClick").hide();
+      $("#slothClick").hide();
+      $("#ligerClick").hide();
     } else {
       $("#sloth").show();
       $("#quokka").hide();
       $("#liger").hide();
-    }  
+    } 
+  });
+
+  $("#liger").click(function() {
+    $("#ligerClick").show();
+    $("#slothClick").hide();
+    $("#quokkaClick").hide();
+  });
+  $("#sloth").click(function() {
+    $("#slothClick").show();
+    $("#ligerClick").hide();
+    $("#quokkaClick").hide();
   });
 });
